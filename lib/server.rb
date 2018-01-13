ce8b12ffbea6cf5c6b33ef9f199e78c189da8178
Daniel Varcas <daniel.varcas@gmail.com>
2018-01-13 12:53:05 +0000

    reworking cipher. not functional.

--- lib/server.rb
+++ lib/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'cipher'
 
 get '/' do
 	text = Text.new(text)
@@ -6,50 +7,6 @@ get '/' do
 	result = # Encrypter.new(text, factor) <--- no
 	message = result
 	erb :index, :locals => {:message = message}
-end
-
-module Encrypter
-	attr_reader :text, :factor
-
-	def account_for_full_loops_through_alphabet(factor)
-		factor % 26
-	end
-
-	def to_letters(text)
-		text = text.split(//)
-		text each do |letter|
-			letter = letter.ord
-		end
-	end
-
-	def is_a_letter(letter)
-		(letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122)
-	end
-
-	def is_uppercase(letter)
-		x = letter
-		if (x >= 65 && x <= 90)
-			false #lowercase
-		elsif (x >= 97 && x <= 122)
-			true
-		end
-	end
-
-	def encrypt(text) #encrypts a single letter
-		letters = text.to_letters
-		letters each do |letter|
-			if letter.is_a_letter
-				if letter.is_uppercase
-					if factor.account_for_full_loops_through_alphabet ### GOT TO HERE.
-			letter.is_a_letter ? letter.check_if_uppercase
-			letter.is_uppercase ? 
-		end
-	end
-end
-
-class TextToEncrypt
-	include Encrypter
-	attr_reader :text
 end
 
 
