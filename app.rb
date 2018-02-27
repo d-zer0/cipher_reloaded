662f80de646fce88d590716a437b3f0aebd5cc7c
Daniel Varcas <daniel.varcas@gmail.com>
2018-02-27 22:01:26 +0000

    all tests pass

--- app.rb
+++ app.rb
@@ -6,6 +6,18 @@ get '/' do
 	erb :index, { :locals => params }
 end
 
-def encrypt(text)
-	text.split("").map { |letter|  (letter =~ /[A-Za-z]/ ? letter.ord+1 : letter.ord).chr}.join
+def encrypt(text, shift=1)
+	@shift = shift % 26
+
+	text.split("").map do |character|
+		if character =~ /[A-Za-z]/
+			if (character.ord + @shift).chr =~ /[A-Za-z]/
+				(character.ord + @shift).chr
+			else
+				(character.ord + (shift % 26) - 26).chr
+			end
+		else
+			character
+		end
+	end.join
 end
