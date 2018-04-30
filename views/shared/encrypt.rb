a5ebb455f63cb03df8fe500bef7cab4c28641af1
Daniel Varcas <daniel.varcas@gmail.com>
2018-04-30 20:23:29 +0100

    Application works

--- views/shared/encrypt.rb
+++ views/shared/encrypt.rb
@@ -1,12 +1,10 @@
 def encrypt(text, shift) 
 	if (text && shift) != nil
-		@text = text
-		@shift = shift % 26
-
+		shift = shift.to_i % 26
 		text.split("").map do |character|
 			if character =~ /[A-Za-z]/
-				if (character.ord + @shift).chr =~ /[A-Za-z]/
-					(character.ord + @shift).chr
+				if (character.ord + shift).chr =~ /[A-Za-z]/
+					(character.ord + shift).chr
 				else
 					(character.ord + (shift % 26) - 26).chr
 				end
